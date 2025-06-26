82e33092a5058e7b0e8532fc3bd490f22950dde4
Antonio Lago <anto.lago.1212@gmail.com>
2025-06-26 12:39:12 +0200

    Primer commit

---
+++ ALB/src/test/java/es/ufv/dis/back/final2025/ALB/AlbApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.final2025.ALB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
